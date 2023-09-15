0e95973e2c2c77cc470e6f480ef10faa53087fd5
JiachengMa <mjc020928@outlook.com>
2023-09-15 14:24:09 +0800

    Update test_fbcca.m

--- src/test_fbcca.m
+++ src/test_fbcca.m
@@ -36,19 +36,19 @@ if ~exist('num_fbs', 'var') || isempty(num_fbs), num_fbs = 5; end
 
 fb_coefs = [1:num_fbs].^(-1.25)+0.25;
 
-[num_targs, ~, num_smpls] = size(eeg);
-y_ref = cca_reference(list_freqs, fs, num_smpls, num_harms);
-for targ_i = 1:1:num_targs
-     test_tmp = squeeze(eeg(targ_i, :, :));
-     for fb_i = 1:1:num_fbs
-         testdata = filterbank(test_tmp, fs, fb_i);
-         for class_i = 1:1:num_targs
-             refdata = squeeze(y_ref(class_i, :, :));
-             [~,~,r_tmp] = canoncorr(testdata', refdata');
-             r(fb_i, class_i) = r_tmp(1,1);
+[num_targs, ~, num_smpls] = size(eeg); % 通道数,样值数,样本人数
+y_ref = cca_reference(list_freqs, fs, num_smpls, num_harms); % 获取参考信号
+for targ_i = 1:1:num_targs % eeg通道数
+     test_tmp = squeeze(eeg(targ_i, :, :)); % 删除长度为1的维度
+     for fb_i = 1:1:num_fbs % 滤波器数
+         testdata = filterbank(test_tmp, fs, fb_i); % 每次用不同的带通得到不同频段的分量
+         for class_i = 1:1:num_targs % 
+             refdata = squeeze(y_ref(class_i, :, :)); % 
+             [~,~,r_tmp] = canoncorr(testdata', refdata'); % 计算典型相关系数
+             r(fb_i, class_i) = r_tmp(1,1); % 取相关值最大的那个结果 \lambda^2
          end % class_i
      end % fb_i
-     rho = fb_coefs*r;
+     rho = fb_coefs*r; % 计算 \rho_n
     [~, tau] = max(rho);
     results(targ_i) = tau;
 end % targ_i
@@ -60,10 +60,10 @@ function [ y_ref ] = cca_reference(list_freqs, fs, num_smpls, num_harms)
 % function [ y_ref ] = cca_reference(listFreq, fs,  nSmpls, nHarms)
 % 
 % Input:
-%   listFreq        : List for stimulus frequencies
-%   fs              : Sampling frequency
-%   nSmpls          : # of samples in an epoch
-%   nHarms          : # of harmonics
+%   list_freqs        : List for stimulus frequencies % 基频列表
+%   fs              : Sampling frequency % 采样率
+%   num_smpls          : # of samples in an epoch % 采样点数
+%   num_harms          : # of harmonics % 谐波次数
 %
 % Output:
 %   y_ref           : Generated reference signals
@@ -91,15 +91,15 @@ end
 
 if ~exist('num_harms', 'var') || isempty(num_harms), num_harms = 3; end
 
-num_freqs = length(list_freqs);
-tidx = (1:num_smpls)/fs;
+num_freqs = length(list_freqs); % 基频数量
+tidx = (1:num_smpls)/fs; % 时间序列
 for freq_i = 1:1:num_freqs
     tmp = [];
-    for harm_i = 1:1:num_harms
-        stim_freq = list_freqs(freq_i);
+    for harm_i = 1:1:num_harms % 谐波次数
+        stim_freq = list_freqs(freq_i); % 本次处理的基频
         tmp = [tmp;...
             sin(2*pi*tidx*harm_i*stim_freq);...
-            cos(2*pi*tidx*harm_i*stim_freq)];
+            cos(2*pi*tidx*harm_i*stim_freq)]; % tmp [2*num_harms,num_smpls]
     end % harm_i
-    y_ref(freq_i, 1:2*num_harms, 1:num_smpls) = tmp;
+    y_ref(freq_i, 1:2*num_harms, 1:num_smpls) = tmp; % y_ref(基频数,2*最高次谐波阶数,采样点数)
 end % freq_i
